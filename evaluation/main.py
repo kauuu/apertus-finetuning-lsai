19b67bc96278b15307b5aa922f1c7fd56e624f3a
Kaushik Karthikeyan <kkarthikeyan@clariden-ln004.cscs.ch>
2025-11-27 17:52:18 +0100

    vllm image added, but still need to do more work to run our finetuned model

--- evaluation/main.py
+++
@@ -1,9 +0,0 @@
-from vllm import LLM, SamplingParams
-
-llm = LLM('kkaushik02/apertus_lora_r32_slds', gpu_memory_utilization=0.7)
-params = SamplingParams(max_tokens=128, temperature=0.7)
-
-outputs = llm.generate(["What is special?"], params)
-
-print(ouputs[0].outputs[0].text.strip())
-
---
+++ evaluation/vllm/README.md
@@ -0,0 +1,3 @@
+# How to get the sqsh file
+
+You need to build the Dockerfile to get the image to run vllm!
